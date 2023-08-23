44afd6622871a08ed027b21c128df0d59547a37b
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-08-23 16:08:33 +0200

    OXDEV-7153: Update version switch

--- conf.py
+++ conf.py
@@ -98,7 +98,10 @@ html_context = {
         ('5.0', 'https://docs.oxid-esales.com/interfaces/graphql/en/5.0/'),
         ('5.1', 'https://docs.oxid-esales.com/interfaces/graphql/en/5.1/'),
         ('5.2', 'https://docs.oxid-esales.com/interfaces/graphql/en/5.2/'),
-        ('6.0', 'https://docs.oxid-esales.com/interfaces/graphql/en/6.0/')
+        ('6.0', 'https://docs.oxid-esales.com/interfaces/graphql/en/6.0/'),
+        ('7.0', 'https://docs.oxid-esales.com/interfaces/graphql/en/7.0/'),
+        ('8.0', 'https://docs.oxid-esales.com/interfaces/graphql/en/8.0/'),
+        ('latest', 'https://docs.oxid-esales.com/interfaces/graphql/en/latest/')
     ],
     'theme_logo_only': True,
     'show_sphinx': False,
